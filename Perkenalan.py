b2aef8d20054d249d9845ddc1401842604c452f7
Riki Irawan <93033636+BlueBear2113@users.noreply.github.com>
2021-11-14 16:29:58 +0700

    Add files via upload

---
+++ Perkenalan.py
@@ -0,0 +1,7 @@
+Nama_orang = input('Siapa kamu ? :')
+awal = input("Apakah kamu mau berkenalan dengan saya? : ")
+
+if awal :
+    print (f'Halo {Nama_orang}, kenalin saya riki irawan biasa dipanggil riki, saya berasal dari jember dan sekarang lagi kuliah di Universitas Jember, salam kenal {Nama_orang} :)')
+
+    
