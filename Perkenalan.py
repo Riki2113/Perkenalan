f33289d71c4dfe1dbf45b84e30b4663ee152b162
Riki Irawan <93033636+BlueBear2113@users.noreply.github.com>
2021-11-14 16:30:59 +0700

    Delete Perkenalan.py

--- Perkenalan.py
+++
@@ -1,7 +0,0 @@
-Nama_orang = input('Siapa kamu ? :')
-awal = input("Apakah kamu mau berkenalan dengan saya? : ")
-
-if awal :
-    print (f'Halo {Nama_orang}, kenalin saya riki irawan biasa dipanggil riki, saya berasal dari jember dan sekarang lagi kuliah di Universitas Jember, salam kenal {Nama_orang} :)')
-
-    
